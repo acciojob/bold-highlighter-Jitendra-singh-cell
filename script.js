a0442f69d8e54937bce357826353de58e516c17c
Jitendra-singh-cell <jitendrasingh9x221@gmail.com>
2024-11-04 22:21:08 +0500

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
+// Function to highlight bold words by changing their color to green
 function highlight() {
-    //Write your code here
-
-
+    const boldWords = document.querySelectorAll('strong');
+    
+    boldWords.forEach((word) => {
+        word.style.color = 'green';
+    });
 }
 
-
 function return_normal() {
-    //Write your code here
-
+    const boldWords = document.querySelectorAll('strong');
     
+    boldWords.forEach((word) => {
+        word.style.color = 'black';
+    });
 }
